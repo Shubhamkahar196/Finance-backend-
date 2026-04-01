f8532b98c2910060e3d0b01a6d3c1a147291c2cc
Shubham kahar <Shubhamkahar196@gmail.com>
2026-04-01 18:29:53 +0530

    added getMe

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -130,3 +130,25 @@ export const logoutUser = (req,res)=>{
     message: "Logged out successfully"
   });
 }
+
+
+// get current user
+
+export const getMe = async(req,res)=>{
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+
+        if(!user){
+             return res.status(404).json({
+        message: "User not found"
+      }); 
+        }
+
+
+    res.status(200).json({ user });
+    } catch (error) {
+        res.status(500).json({
+      message: "Server Error"
+    });
+    }
+}
