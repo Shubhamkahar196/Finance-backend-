a007559d175954e043f3ce961177332ed92a20be
Shubham kahar <Shubhamkahar196@gmail.com>
2026-04-01 18:36:20 +0530

    added authRoutes

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
+
 import express from 'express'
+import cookieParser from 'cookie-parser';
+
+
 import connectDb from './config/db.js';
+import authRoutes from './routes/auth.routes.js'
+
 
 const app = express();
 const PORT = process.env.PORT || 8000
 connectDb();
 
+app.use(cookieParser());
+
+
+app.use("api/auth",authRoutes);
 
 app.listen(PORT,()=>{
     console.log(`Server is running on ${PORT}`);
